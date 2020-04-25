a7cea7dc9e87742796a9edca9275a4d4dee80b06
Julia Margarita Jimenez Herrera <maggie.jh99@gmail.com>
2020-04-25 16:28:53 -0500

    Finishing lab

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require( 'express' );
 const uuidv4 = require("uuid/v4");
 const bodyParser = require('body-parser');
+const morgan = require( 'morgan' );
 
 const app = express();
 const jsonParser = bodyParser.json();
+const TOKEN = "2abbf7c3-245b-404f-9473-ade729ed4653";
 
+app.use( morgan( 'dev' ) );
+
+
+
+function validate( req, res, next ){
+    
+    let token = req.headers.bookapikey;
+    let tokenB = req.headers.authorization;
+    let tokenP = req.query.apikey;
+    let apikey = false;
+    let bearer = false;
+    let param = false;
+
+    if(token === TOKEN){
+        apikey = true;
+    }
+    else if (tokenP === TOKEN){
+        param = true;
+    }
+    else if(tokenB === `Bearer ${TOKEN}`){
+        bearer = true;
+    }
+    else {
+        res.statusMessage = "The 'authorization' TOKEN is invalid.";
+        return res.status( 401 ).end();
+    }
+
+    next();
+}
+
+app.use( validate );
 
 let bookmarks = [
     {
@@ -24,20 +57,13 @@ let bookmarks = [
 ];
 
 app.get( '/bookmarks', ( req, res ) => {
-    console.log( "Getting all bookmarks" );
 
     return res.status( 200 ).json( bookmarks );
-    console.log(id());
 });
 
 app.get( '/bookmark', ( req, res ) => {
-    console.log( 'Getting bookmark by title' );
-
-    console.log( req.query );
 
     let title = req.query.title;
-
-    console.log(title);
 
     if ( !title ){
         res.statusMessage = "Please send the title as parameter";
@@ -61,7 +87,6 @@ app.get( '/bookmark', ( req, res ) => {
 });
 
 app.post( '/bookmarks', jsonParser, ( req, res ) =>{
-    console.log('Adding a bookmark');
     
     let title = req.body.title;
     let description = req.body.description;
@@ -113,6 +138,48 @@ app.delete('/bookmark/:id', (req, res) =>{
     bookmarks.splice( itemToRemove, 1 );
     return res.status( 200 ).end();
 });
+
+app.patch('/bookmark/:id', jsonParser, (req, res ) => {
+    
+    let title = req.body.title;
+    let description = req.body.description;
+    let url = req.body.url;
+    let rating = req.body.rating;
+    let id = req.params.id;
+    if( !id ){
+        res.statusMessage = "Please send the 'id'";
+        return res.status( 406 ).end();
+    }
+
+    let result = bookmarks.find( (bookmark) => {
+        if(bookmark.id === id){
+            return bookmark;
+        }
+    });
+
+    if( !result ){
+        res.statusMessage = "There is no bookmark with the id passed";
+        return res.status( 409 ).end();
+    }
+
+    if(title){
+        result.title = title
+    }
+
+    if(description){
+        result.description = description;
+    }
+
+    if(url){
+        result.url = url;
+    }
+
+    if(rating){
+        result.rating = rating;
+    }
+
+    return res.status( 202 ).json(result);
+})
 
 app.listen( 8080, () => {
     console.log( 'This server is running on port 8080' );
